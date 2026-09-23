53b391e47263032df36dd093a6e3778ab840384a
PeruDoxer5 <perudoxer@gmail.com>
2026-09-23 16:08:55 -0300

    fix: add Android reception app icons

--- public/recepcion-sw.js
+++ public/recepcion-sw.js
@@ -1,5 +1,5 @@
-const CACHE = "fundo-recepcion-v1";
-const APP_SHELL = ["/", "/recepcion.webmanifest", "/recepcion-icon.svg"];
+const CACHE = "fundo-recepcion-v2";
+const APP_SHELL = ["/", "/recepcion.webmanifest", "/recepcion-icon-192.png", "/recepcion-icon-512.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)));
